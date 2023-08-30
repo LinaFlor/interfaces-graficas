b31d071b917cd681075b6dff5233e66e02335244
root <mgiraudo@ies21.edu.ar>
2023-08-30 19:00:42 -0300

    segunda version

---
+++ src/NewClass.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Alumno
+ */
+public class NewClass {
+    
+    public void hola () {
+        System.out.print("holaaa");
+    }
+}
